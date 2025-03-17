8c3d9cffdabc9d0d0b8bc6468881b398b13ba0af
yogesh-nade <yogeshnade34@gmail.com>
2025-03-17 22:11:58 +0530

    Create __init__.py

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+  
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "db24c608640f5034b30b8e1e1eb5618ed0ffdbf5"
+app.config["MONGO_URI"] = ""
+
+# mongodb database
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+from application import routes
